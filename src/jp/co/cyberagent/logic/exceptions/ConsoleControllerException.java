1bb951a21d54a50f7174d59fb037408d4be6abe4
phucnh <nguyenhongphuc.hut@gmail.com>
2015-01-18 12:05:23 +0700

    add console controller exception

---
+++ src/jp/co/cyberagent/logic/exceptions/ConsoleControllerException.java
@@ -0,0 +1,12 @@
+package jp.co.cyberagent.logic.exceptions;
+
+/**
+ * Created by phucnh on 15/01/18.
+ */
+public class ConsoleControllerException extends ControllerException {
+
+    public ConsoleControllerException(String s) {
+        super(s);
+    }
+
+}
